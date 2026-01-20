447707c7c78cf18140b54eda0d219f34f17a9ff4
Evan Xu <evanxu365@gmail.com>
2026-01-19 21:03:42 -0800

    Read files

--- regression_model.py
+++ regression_model.py
@@ -6,10 +6,20 @@ ALPHA = 1
 
 
 def main():
-    pass
+    print(read_file(FILE))
 
 
-
+def read_file(file=FILE):
+    '''Returns the x and y values'''
+    x_values = []
+    y_values = []
+    with open(file, newline='') as f:
+        data = csv.reader(f)
+        header = next(data)
+        for row in data:
+            x_values.append(row[0])
+            y_values.append(row[1])
+    return x_values, y_values
 
 
 if __name__ == '__main__':
