009cd85d55fff3369a75568677c7d45bfdf81df0
Evan Xu <evanxu365@gmail.com>
2026-01-27 21:49:21 -0800

    Allowed multiple variables

--- regression_model.py
+++ regression_model.py
@@ -1,4 +1,3 @@
-import csv
 import numpy as np
 import time
 import sys
@@ -6,77 +5,65 @@ import sys
 
 FILE = sys.argv[1]
 ALPHA = 0.00025
+ITERATIONS = 100000
 
 
 def main():
     time_start = time.time()
-    x_values, y_values = read_file(FILE)
-    weight = 0
-    bias = 0
-    for i in range(2000000):
-        weight, bias = gradient_descent(weight, bias, x_values, y_values, ALPHA)
-        if i % 100000 == 0:
-            print(i, cost(weight, bias, x_values, y_values))
-            print(weight, bias)
+    X, y = read_file(FILE)
+
+    m, n = X.shape
+    w = np.zeros(n)
+    b = 0
+    
+    for i in range(ITERATIONS):
+        w, b = gradient_descent(w, b, X, y, ALPHA)
+        if i % 10000 == 0:
+            print(i, find_cost(w, b, X, y))
+            print(w, b)
     time_end = time.time()
     print(f"Time: {1000*(time_end - time_start):.4f} ms ")
 
 
 def read_file(file=FILE):
     """Returns the x and y values"""
-    x_values = []
-    y_values = []
-    with open(file, newline="") as f:
-        data = csv.reader(f)
-        header = next(data)
-        for row in data:
-            x_values.append(float(row[0]))
-            y_values.append(float(row[1]))
-    return np.array(x_values), np.array(y_values)
+    data = np.loadtxt(file, delimiter=',', skiprows=1)
+    X = data[:, :-1] # A 2d array of all values besides the last
+    y = data[:, -1] # A 1d array of the last values
+    
+    return X, y
 
 
-def y_hat(weight, bias, x):
+def y_hat(w, b, X):
     """Give the predicted y values"""
-    return weight * x + bias
+    return X @ w + b
 
 
-def cost(weight, bias, x_values, y_values):
+def find_cost(w, b, X, y):
     """Find the cost of error"""
-    length = x_values.shape[0]
-    total = 0
-
-    for i in range(length):
-        val = np.dot(x_values[i], weight) + bias
-        total += (val - y_values[i]) ** 2
-
-    total /= 2 * length
-
-    return total
+    m = X.shape[0]
+    errors = y_hat(w, b, X) - y
+    cost = (errors @ errors) / (2 * m)
+    return cost
 
 
-def compute_gradient(weight, bias, x_values, y_values, total):
+def compute_gradient(w, b, X, y):
     """Return the computed gradient"""
-    w = 0
-    b = 0
-    for x, y in zip(x_values, y_values):
-        w += (y_hat(weight, bias, x) - y) * x
-        b += y_hat(weight, bias, x) - y
-    return w / total, b / total
+    m = X.shape[0]
+    errors = y_hat(w, b, X) - y
+
+    dw = (X.T @ errors) / m
+    db = errors.sum() / m
+
+    return dw, db
 
 
-def gradient_descent(weight, bias, x_values, y_values, alpha=ALPHA):
-    total_values = x_values.shape[0]
-    w = weight
-    b = bias
-    gradient = list(
-        map(
-            lambda x: x * alpha,
-            compute_gradient(weight, bias, x_values, y_values, total_values),
-        )
-    )
+def gradient_descent(w, b, X, y, alpha=ALPHA):
+    dw, db = compute_gradient(w, b, X, y)
 
-    w -= gradient[0]
-    b -= gradient[1]
+    w -= alpha * dw
+    b -= alpha * db
+
     return w, b
 
 
